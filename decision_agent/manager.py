b8ee047ebc430061ebee2a95afeca5e93c2d2d1b
Aleksanderzerokder <aleksanderzerokder@gmail.com>
2025-05-06 09:54:17 +0300

    Delete decision_agent directory

--- decision_agent/manager.py
+++
@@ -1,9 +0,0 @@
-class DecisionAgent:
-    def __init__(self, results):
-        self.results = results
-
-    def generate_summary(self):
-        return {
-            "recommendations": [r.get("recommendation") for r in self.results.values()],
-            "raw_data": self.results
-        }
